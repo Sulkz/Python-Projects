919bae96381ddb7937c98f6dfbe1877a4ee49693
Reuel <Rcigbokweonuigbo1@sheffield.ac.uk>
2023-06-16 00:46:20 +0100

    created starting point including checking length and country code

---
+++ practice1.py
@@ -0,0 +1,24 @@
+import time
+import random
+
+print("Welcome to the beta caller id")
+sentence = str(input("Enter your phone number: "))
+
+#makes sure number is standard length
+length = len(sentence)
+
+if length != 13:
+    print("Incorrect amount of numbers")
+else:
+    print("continue")
+
+time.sleep(1.75)
+#makes sure number has Uk standard 
+trueOrFalse = "+44" in sentence
+randNum = random.randrange(1,100)
+
+time.sleep(1.75)
+if trueOrFalse == True:
+    print("Welcome here is your qeue number: ", randNum )
+else:
+    print("Um idk u could be any country")
