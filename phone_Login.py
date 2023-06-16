6c1dbc7fc80fd13cea4c917c22a5cfc7a778274d
Reuel <Rcigbokweonuigbo1@sheffield.ac.uk>
2023-06-16 11:37:54 +0100

    learnt new .append function now can type in ur number and it will be stored  :D

--- phone_Login.py
+++ phone_Login.py
@@ -1,33 +1,45 @@
 import time
 import random
-count  = 0
 
+count = 0
+numbersLog = []
 
-def login(setence):
-    print("Welcome to the beta caller id")
-    sentence = str(input("Enter your phone number: "))
+while count < 5:
+    def login():
+        print("Welcome to the beta caller id")
+        sentence = str(input("Enter your phone number: "))
 
-    #makes sure number is standard length
-    length = len(sentence)
+        # makes sure number is standard length
+        length = len(sentence)
 
+        if length != 13 and length != 11:
+            print("Incorrect amount of numbers")
+            exit()
+        else:
+            print("continue")
 
-    if length != 13 and length != 11:
-        print("Incorrect amount of numbers")
-    else:
-        print("continue")
+        time.sleep(1.75)
+        # makes sure number has UK standard
+        trueOrFalse = "+44" in sentence or "07" in sentence
 
-    time.sleep(1.75)
-    #makes sure number has Uk standard 
-    trueOrFalse = "+44" in sentence or "07" in sentence
+        randNum = random.randrange(1, 100)
 
-    randNum = random.randrange(1,100)
+        time.sleep(1.75)
+        if trueOrFalse:
+            print("Welcome! Here is your queue number:", "#", randNum)
+            time.sleep(1.3)
+        else:
+            print("Um, I don't know. You could be from any country.")
 
-    time.sleep(1.75)
-    if trueOrFalse:
-        print("Welcome here is your qeue number: ", "#",randNum )
-        time.sleep(1.3)
-    else:
-        print("Um idk u could be any country")
+        return sentence
 
-login(setence="")
+    sentence = login()
+    print(count)
 
+    numbersLog.append(sentence)
+    print(numbersLog)
+
+    if count == 3:
+        break
+
+    count += 1
