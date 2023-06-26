c933b2af13fc1c2769828ce4b11485f3ebb48235
Reuel <Rcigbokweonuigbo1@sheffield.ac.uk>
2023-06-26 14:29:16 +0100

    add a patter for the email to match

--- regular_expression.py
+++ regular_expression.py
@@ -2,12 +2,25 @@ import re
 
 def validate_Email():
     userInput = str(input("Enter your email: "))
+
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    if re.match(pattern, userInput):
+        print("email format correct")
+    
+
     gmail_Valid = re.findall("@gmail.com", userInput)
     yahoo_Valid = re.findall("@yahoo.com", userInput)
+    outlook_Valid = re.findall("@outlook.com", userInput)
 
-    if (gmail_Valid, yahoo_Valid):
-        print("match found")
+    if (gmail_Valid):
+        print("I detect a gmail account")
+    elif (yahoo_Valid):
+        print("i detect a yahoo account")
+    elif (outlook_Valid):
+        print("i detect a Outlook account")
     else:
-        print("email not in correct format")
+        print("ermm i dont kniow which u are mate")
+    
+
 
 validate_Email()
